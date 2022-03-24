aee9ba02c39bbb9d78c82a31883f8d3cf66378c8
verjoxd <101992548+verjoxd@users.noreply.github.com>
2022-03-23 20:44:06 -0400

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -22,8 +22,7 @@ int main()
 
     point plat[20];
 
-    for (int i=0;i<10;i++)
-      {
+    for (int i=0;i<10;i++){
        plat[i].x=rand()%400;
        plat[i].y=rand()%533;
       }
@@ -31,46 +30,46 @@ int main()
     int x=100,y=100,h=200;
     float dx=0,dy=0;
 
-    while (app.isOpen())
-    {
+    while (app.isOpen()){
         Event e;
-        while (app.pollEvent(e))
-        {
+        while (app.pollEvent(e)){
             if (e.type == Event::Closed)
                 app.close();
-        }
+        	}
 
-    if (Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
-    if (Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
+  		 if (Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
+   		 if (Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
 
-    dy+=0.2;
-    y+=dy;
-    if (y>500)  dy=-10;
+    	dy+=0.2;
+    	y+=dy;
+    		if (y>500)  dy=-10;
 
-    if (y<h)
-    for (int i=0;i<10;i++)
-    {
-      y=h;
-      plat[i].y=plat[i].y-dy;
-      if (plat[i].y>533) {plat[i].y=0; plat[i].x=rand()%400;}
-    }
+    		if (y<h)
+    			for (int i=0;i<10;i++){
+    				 y=h;
+     				 plat[i].y=plat[i].y-dy;
+    			  if (plat[i].y>533) {plat[i].y=0; plat[i].x=rand()%400;}
+    			}
 
-    for (int i=0;i<10;i++)
-     if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
-      && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-10;
+   			 for (int i=0;i<10;i++)
+   				  if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
+    					  && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-10;
 
-    sPers.setPosition(x,y);
+  					sPers.setPosition(x,y);
+   					//esto deberia cerrar la ventana
+					sf::Vector2f pop=sPers.getPosition();
+   					if (pop.y <= 150){
+						app.close();
+						}
+					app.draw(sBackground);
+    					app.draw(sPers);
+    					for (int i=0;i<10;i++){
+    						sPlat.setPosition(plat[i].x,plat[i].y);
+    						app.draw(sPlat);
+  						  }
 
-    app.draw(sBackground);
-    app.draw(sPers);
-    for (int i=0;i<10;i++)
-    {
-    sPlat.setPosition(plat[i].x,plat[i].y);
-    app.draw(sPlat);
-    }
-
-    app.display();
-}
+   				 app.display();
+				}
 
     return 0;
 }
